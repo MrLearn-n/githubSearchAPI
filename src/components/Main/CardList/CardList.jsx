5b3c4318333c60184c4444ac07344e386005bbb6
MrLearn <learnmr67@gmail.com>
2023-03-26 07:10:52 +0300

    добавил кнопку для перехода на отдельную страницу с проектом

--- src/components/Main/CardList/CardList.jsx
+++ src/components/Main/CardList/CardList.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import {MdOutlineStar} from 'react-icons/md';
 import {BsFillEyeFill} from 'react-icons/bs';
-import style from './CardList.module.css';
 import CommentBox from './CommentBox/CommentBox';
+import style from './CardList.module.css';
 
 
 
 
-export default function CardList({name, img, auth, countStar, countWatch, linkRepo, linkAuthor}) {
+
+
+export default function CardList({name, img, auth, countStar, countWatch, linkRepo, linkAuthor, index}) {
     return (
         <div className={style.card_content}>
                 <p className={style.card_content__name}><a href={linkRepo}>{name}</a></p>
@@ -17,14 +20,19 @@ export default function CardList({name, img, auth, countStar, countWatch, linkRe
                     </div>
                     <p className={style.card_content__auth___authorName}><a href={linkAuthor}>{auth}</a></p>
                 </div>
-                <div className={style.card_content__counts}>
-                    <div className={style.counts_star}>
-                        <MdOutlineStar />
-                        <p className={style.valueCount}>{countStar}</p>
-                    </div>
-                    <div className={style.counts_watch}>
-                        <BsFillEyeFill />
-                        <p className={style.valueCount}>{countWatch}</p>
+                <div className={style.card_content__info}>
+                    <div className={style.card_content__info___counts}>
+                        <div className={style.counts_star}>
+                            <MdOutlineStar />
+                            <p className={style.valueCount}>{countStar}</p>
+                        </div>
+                        <div className={style.counts_watch}>
+                            <BsFillEyeFill />
+                            <p className={style.valueCount}>{countWatch}</p>
+                        </div>
+                        <NavLink to={`/project/${index}`} className={style.btnInfo}>
+                            Подробнее
+                        </NavLink>
                     </div>
                 </div>
                 <CommentBox />
