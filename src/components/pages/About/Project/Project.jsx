76f1d58369b9076bcd39d7944be7d68993d49776
MrLearn <learnmr67@gmail.com>
2023-03-27 08:42:25 +0300

    сделал вывод данных о пользователе и стили для страницы

--- src/components/pages/About/Project/Project.jsx
+++ src/components/pages/About/Project/Project.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
-import {MdOutlineStar} from 'react-icons/md';
-import {BsFillEyeFill} from 'react-icons/bs';
+import { MdOutlineStar } from 'react-icons/md';
+import { BsFillEyeFill } from 'react-icons/bs';
 import style from './Project.module.css'
 
 
-export default function Project({img, name, authName, description, lang, countWatch, countStar}) {
+export default function Project({ img, name, authName, description, lang, url, countWatch, countStar }) {
     return (
-        <div className=''>
-            <div className=''>
-                <img src={img} alt={name} />
-            </div>
-            <div className=''>
-                <div className=''>
-                    <p>{name}</p>
-                    <p>{authName}</p>
-                    <p>{description}</p>
+        <div className='project'>
+            <div className={style.project_info}>
+                <div className={style.project_info__img}>
+                    <img src={img} alt={name} />
                 </div>
-                <div className=''>
-                    <p>{lang}</p>
-                </div>
-                <div className=''>
-                    <div className=''>
-                        <MdOutlineStar />
-                        <p className=''>{countStar}</p>
+                <div className={style.project_info__text}>
+                    <div className={style.info__text}>
+                        <p>Название проекта: {name}</p>
+                        <p>Имя автора: {authName}</p>
+                        <p>Описание: {description}</p>
                     </div>
                     <div className=''>
-                        <BsFillEyeFill />
-                        <p className=''>{countWatch}</p>
+                        <p>Языки: {lang}</p>
+                    </div>
+                    <div className={style.project_info__text___counts}>
+                        <div className={style.counts_star}>
+                            <MdOutlineStar />
+                            <p className={style.valueCount}>{countStar}</p>
+                        </div>
+                        <div className={style.counts_watch}>
+                            <BsFillEyeFill />
+                            <p className={style.valueCount}>{countWatch}</p>
+                        </div>
+                    </div>
+                    <div className=''>
+                        <p>URL: <a href={url}> {url} </a></p>
                     </div>
                 </div>
             </div>
